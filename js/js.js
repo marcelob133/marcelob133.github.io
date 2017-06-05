44486f6a2117be9d3bda82d0c90aa0c7cf3931bb
Marcelo B H <marceloborellihegedus@gmail.com>
2017-06-05 18:29:44 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,6 @@
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('#typed', {
+        stringsElement: document.getElementById('typed-strings'),
+        typeSpeed: 5, loop: true,	
+      });
+  });
